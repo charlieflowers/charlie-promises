07a14c27cf9f3237325ce0d43479563133505748
Charlie Flowers <cflowers@gmail.com>
2016-05-24 03:11:12 -0400

    the interface makes it impossible for me to tell if i am makling progress or not

--- test/my-adapter.js
+++ test/my-adapter.js
@@ -1,21 +1,44 @@
 module.exports = {
-    resolved: value =>  {
-        
-    },
-    
+    resolved: resolved,
+
     rejected: reason => {
-        
+
     },
-    
-    deferred: ()=> {
+
+    deferred: () => {
         return {
             promise: {},
             resolve: value => {
-                
+
             },
             reject: reason => {
-                
+
             }
         }
     }
+}
+
+function newPromise() {
+    return {
+        successCbs: [],
+        errorCbs: [],
+        state: 'unsettled',
+        value: undefined,
+        resolve: val => {
+            this.val = val;
+            this.state = 'resolved';
+        },
+        then: (successCb, errorCb) => {
+            if (successCb) { this.successCbs.push(cb) };
+            if (errorCb) { this.errorCbs.push(cb) };
+            return this;
+        }
+    }
+}
+
+function resolved(value) {
+    var p = newPromise();
+    p.resolve(value);
+
+    return p;
 }
